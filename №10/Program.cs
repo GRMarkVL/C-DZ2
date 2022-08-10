e89e63306c7f6789896bf4d04d062e391c531ec7
GRMarkVL <ClimatFL@gmail.com>
2022-08-10 09:22:00 +0300

    решил задачу 10

---
+++ №10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+
+void MaxDigit(int num)
+{
+    
+    int number = (num /10)%10; 
+    Console.WriteLine($"{number}");
+}
+
+MaxDigit(number); 
