3a3943a9e9db9c3984d517e8260977eda64f1981
GRMarkVL <ClimatFL@gmail.com>
2022-08-20 20:39:26 +0300

    Сделал домашнее задание, задачи №34, №36, №38

---
+++ №36/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. 
+//  Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 
+
+
+Console.Clear();
+
+int[] CreateArrayRndInt(int size,int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] GetSumPosNegElem(int[] array)
+{
+    int sumPos = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (i % 2 != 0) sumPos += array[i];
+    }
+
+    return new int[] {sumPos};
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(4,-99,99);
+
+PrintArray(arr);
+int[] sumPosNegElem = GetSumPosNegElem(arr);
+Console.WriteLine($"Cумма стоящих на нечётных позициях (индексах): {sumPosNegElem[0]}");
---
+++ №38/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Задайте массив вещественных чисел.
+//   Найдите разницу между максимальным и минимальным элементами массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Math.Round((rnd.NextDouble() * (max-min)+ min),1);
+    }
+    return array;
+}
+double DiffMaxMinElem(double[] array)
+{
+    double maxelem = array[0]; 
+    double minelem = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] > maxelem) maxelem = array[i];
+       if (array[i] < minelem) minelem = array[i];
+    }
+
+    double diffmaxmin = maxelem - minelem;
+
+    return diffmaxmin;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+double[] arr = CreateArrayRndDouble(10, 1, 99);
+double diffMaxMinElem = DiffMaxMinElem(arr);
+
+PrintArray(arr);
+Console.Write($" -> {Math.Round(diffMaxMinElem,1)}");
+Console.WriteLine();
