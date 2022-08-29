1affbbc046132f9ab3cecf7a371c053249daf1ff
GRMarkVL <ClimatFL@gmail.com>
2022-08-30 01:06:30 +0300

    решил задачи 41,43

---
+++ №47/Program.cs
@@ -0,0 +1,105 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+double[,] CreateMatrixRndDouble(int row, int col, int min, int max)
+{
+    double[,] matrix = new double[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrixDouble(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],5} ,");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int Rows()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество строк: ");
+            int mTmp = int.Parse(Console.ReadLine());
+
+            if (mTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return mTmp;
+        }
+        catch 
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int Colums()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество столбцов: ");
+            int nTmp = int.Parse(Console.ReadLine());
+
+            if (nTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return nTmp;
+        }
+        catch 
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int m = Rows();
+
+int n = Colums();
+
+Console.WriteLine();
+
+Console.WriteLine($"m = {m}, n = {n}");
+
+double[,] array2d = CreateMatrixRndDouble(m, n, -10, 10);
+
+PrintMatrixDouble(array2d);
---
+++ №50/Program.cs
@@ -0,0 +1,127 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+Console.Clear();
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void CheckPositiionMatrix(int rowPosition,int columPosition, int[,] array)
+{
+    try
+    {
+        Console.WriteLine($"{rowPosition}, {columPosition} -> {array[rowPosition-1,columPosition-1]}");
+    }
+    catch
+    {
+        Console.WriteLine($"{rowPosition}, {columPosition} -> такого элемента в массиве нет");
+    }
+}
+
+int[,] CreateMatrixRndInt(int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rnd.Next(2,10),rnd.Next(2,10)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min,max);
+        }
+    }
+
+    return matrix;
+}
+
+int RowNumber()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите номер строки: ");
+            int mTmp = int.Parse(Console.ReadLine());
+
+            if (mTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return mTmp;
+        }
+        catch 
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int ColumNumber()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите номер столбца: ");
+            int nTmp = int.Parse(Console.ReadLine());
+
+            if (nTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return nTmp;
+        }
+        catch 
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+System.Console.WriteLine("Введите позиции элемента: ");
+
+Console.WriteLine();
+
+int rowPos = RowNumber();
+
+int columPos = ColumNumber();
+
+Console.WriteLine();
+
+int[,] array2d = CreateMatrixRndInt(1, 10);
+
+PrintMatrix(array2d);
+
+Console.WriteLine();
+
+CheckPositiionMatrix(rowPos,columPos,array2d);
+
+Console.WriteLine();
